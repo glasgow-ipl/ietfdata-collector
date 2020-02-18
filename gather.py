72f145f43590e48a8c671f1a2075e333dc857516
Stephen McQuistin <sm@smcquistin.uk>
2020-02-18 11:55:51 +0000

    Gather people

--- gather.py
+++ gather.py
@@ -27,13 +27,94 @@ import ietfdata.datatracker as DT
 import requests_cache
 import pathlib
 import json
+import requests
+import datetime
+import os
+import shutil
+import dateutil
+import pytz
 
-class PersonEncoder(json.JSONEncoder):
+from typing import Iterator, Optional
+
+class EncodeUsingToJSON(json.JSONEncoder):
     def default(self, object):
-        if isinstance(object, DT.Person):
-            return object.__dict__
-        else:
-            return json.JSONEncoder.default(self, object)
+        return object.toJSON()
+
+
+class SerialisablePersonEvent:
+    type         : str
+    desc         : str
+    time         : str
+
+    def __init__(self, event: DT.PersonEvent):
+        self.type = event.type
+        self.desc = event.desc
+        self.time = event.time
+
+    def toJSON(self):
+        return self.__dict__
+
+     
+class SerialisablePersonAlias:
+    name         : str
+
+    def __init__(self, alias: DT.PersonAlias):
+        self.name = alias.name
+
+    def toJSON(self):
+        return self.__dict__
+
+
+class SerialisableEmail:
+    address      : str
+    time         : str
+    origin       : str
+    primary      : bool
+    active       : bool
+
+    def __init__(self, email: DT.Email):
+        self.address = email.address
+        self.time = email.time
+        self.origin = email.origin
+        self.primary = email.primary
+        self.active = email.active
+
+    def toJSON(self):
+        return self.__dict__
+
+
+class SerialisablePerson:
+    id              : int
+    name            : str
+    name_from_draft : str
+    ascii           : str
+    ascii_short     : Optional[str]
+    user            : str
+    time            : str
+    photo           : str
+    photo_thumb     : str
+    biography       : str
+    consent         : bool
+
+    def __init__(self, person: DT.Person, emails: Iterator[DT.Email], aliases: Iterator[DT.PersonAlias], events: Iterator[DT.PersonEvent]):
+        self.id = person.id
+        self.name = person.name
+        self.name_from_draft = person.name_from_draft
+        self.ascii = person.ascii
+        self.ascii_short = person.ascii_short
+        self.user = person.user
+        self.time = person.time
+        self.photo = person.photo
+        self.photo_thumb = person.photo_thumb
+        self.biography = person.biography
+        self.consent = person.consent
+        self.emails = [SerialisableEmail(email) for email in emails]
+        self.aliases = [SerialisablePersonAlias(alias) for alias in aliases]
+        self.events = [SerialisablePersonEvent(event) for event in events]
+
+    def toJSON(self):
+        return self.__dict__
+
 
 class DataTrackerCollector:
     def __init__(self, use_cache=False):
@@ -42,17 +123,40 @@ class DataTrackerCollector:
                 requests_cache.install_cache("dt_collector_cache")
         pathlib.Path("data").mkdir(parents=True, exist_ok=True)
 
-    def serialise_person(self, person: DT.Person):
+    def get_file_if_changed(self, uri: str, dest_filename: str):
+        r = requests.head(uri)
+        remote_lastmodified = dateutil.parser.parse(r.headers['last-modified'])
+        try:
+            local_lastmodified = pytz.UTC.localize(datetime.datetime.fromtimestamp(os.path.getmtime(dest_filename)))
+        except:
+            local_lastmodified = None
+        if local_lastmodified is None or remote_lastmodified > local_lastmodified:
+            r = requests.get(uri, verify=True)
+            if r.status_code == 200:
+                with open(dest_filename, "wb") as dest_file:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, dest_file)
+
+    def serialise_person(self, person: DT.Person, emails: Iterator[DT.Email], aliases: Iterator[DT.PersonAlias], events: Iterator[DT.PersonEvent]):
         pathlib.Path("data/people/%d" % (person.id)).mkdir(parents=True, exist_ok=True)
         with open("data/people/%d/metadata.json" % person.id, "w") as personMetadataFile:
-            json.dump(person, personMetadataFile, indent=4, cls=PersonEncoder)
+            json.dump(SerialisablePerson(person, emails, aliases, events), personMetadataFile, indent=4, cls=EncodeUsingToJSON)
+        if (person.photo != "None"):
+            pathlib.Path("data/people/%d/media/photo" % (person.id)).mkdir(parents=True, exist_ok=True)
+            self.get_file_if_changed(person.photo, "data/people/%d/media/photo/%s" % (person.id, person.photo.split('/')[-1]))
+        if (person.photo_thumb != "None"):
+            pathlib.Path("data/people/%d/media/photo" % (person.id)).mkdir(parents=True, exist_ok=True)
+            self.get_file_if_changed(person.photo_thumb, "data/people/%d/media/photo/%s" % (person.id, person.photo_thumb.split('/')[-1]))
 
     def gather_people(self):
         pathlib.Path("data/people").mkdir(parents=True, exist_ok=True)
         print("Gathering people..")
         for person in self.dt.people():
-            print(person)
-            self.serialise_person(person)
+            print("Gathering data for %d.." % person.id)
+            emails = self.dt.email_for_person(person)
+            aliases = self.dt.person_aliases(person)
+            events = self.dt.person_events(person)
+            self.serialise_person(person, emails, aliases, events)
 
     def gather(self):
         self.gather_people()
