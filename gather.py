469a992184f967515181f490bce4e0a3215990ef
Stephen McQuistin <sm@smcquistin.uk>
2020-03-04 16:53:53 +0000

    Only pull in people added since the previous run

--- gather.py
+++ gather.py
@@ -41,17 +41,16 @@ class DataTrackerCollector:
 
     def gather_person(self, person: DT.Person):
         try:
-            self.conn.execute("INSERT OR REPLACE INTO person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            self.conn.execute("INSERT OR REPLACE INTO person_person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                             (person.id,
+                             person.time,
                              person.name,
-                             person.name_from_draft,
                              person.ascii,
                              person.ascii_short,
-                             person.user,
-                             person.time,
+                             person.biography,
                              person.photo,
                              person.photo_thumb,
-                             person.biography,
+                             person.name_from_draft,
                              person.consent))
             self.conn.commit()
         except Exception as e:
@@ -145,34 +144,88 @@ class DataTrackerCollector:
                                   person_event.type))
                 self.conn.commit()
             except Exception as e:
-                print(e)
                 print("Did not add person event for person ID %d [%s]" % (person.id, e))
 
 
     def gather_people(self):
-        for person in self.dt.people():
+        # get the most recent datestamp in the table
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM person_person ORDER BY time DESC LIMIT 1");
+        rows = cur.fetchall()
+        since = None
+        if (len(rows) == 1):
+            since = rows[0][1]
+        print(since)
+        if since is not None:
+            people = self.dt.people(since=since)
+        else:
+            people = self.dt.people()
+        for person in people:
             print("Gathering data for %d.." % person.id)
             self.gather_person(person)
-            self.gather_aliases(person)
-            self.gather_emails(person)
-            self.gather_historical_emails(person)
-            self.gather_historical_people(person)
-            self.gather_person_events(person)
+            #self.gather_aliases(person)
+            #self.gather_emails(person)
+            #self.gather_historical_emails(person)
+            #self.gather_historical_people(person)
+            #self.gather_person_events(person)
+
+
+    def gather_document(self, document: DT.Document):
+        try:
+            ad = document.ad
+            shepherd = document.shepherd
+            group = document.group
+            if ad is not None:
+                ad = int(ad.uri.split('/')[-2])
+            if shepherd is not None:
+                shepherd = shepherd.uri.split('/')[-2]
+            if group is not None:
+                group = int(group.uri.split('/')[-2])
+            self.conn.execute("INSERT OR REPLACE INTO document VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                             (document.id,
+                              document.name,
+                              document.title,
+                              document.pages,
+                              document.words,
+                              document.time,
+                              document.expires,
+                              document.type.split('/')[-2],
+                              document.rfc,
+                              document.rev,
+                              document.abstract,
+                              document.internal_comments,
+                              document.order,
+                              ad,
+                              shepherd,
+                              group,
+                              document.stream,
+                              document.intended_std_level,
+                              document.std_level,
+                              document.uploaded_filename,
+                              document.external_url))
+            self.conn.commit()
+        except Exception as e:
+            print("Did not document %s [%s]" % (document.name, e))
+
+
+    def gather_documents(self):
+        for document in self.dt.documents():
+            print("Gather data for %s.." % document.name)
+            self.gather_document(document)
 
 
     def create_tables(self):
         # Person
-        self.conn.execute('''CREATE TABLE IF NOT EXISTS person (id              int  NOT NULL PRIMARY KEY,
-                                                                name            text NOT NULL,
-                                                                name_from_draft text NOT NULL,
-                                                                ascii           text NOT NULL,
-                                                                ascii_short     text,
-                                                                user            text NOT NULL,
-                                                                time            text NOT NULL,
-                                                                photo           text NOT NULL,
-                                                                photo_thumb     text NOT NULL,
-                                                                biography       text NOT NULL,
-                                                                consent         bool NOT NULL)''')
+        self.conn.execute('''CREATE TABLE IF NOT EXISTS person_person (id              int          NOT NULL PRIMARY KEY,
+                                                                       time            datetime     NOT NULL,
+                                                                       name            varchar(255) NOT NULL,
+                                                                       ascii           varchar(255) NOT NULL,
+                                                                       ascii_short     varchar(32)  DEFAULT NULL,
+                                                                       biography       longtext     NOT NULL,
+                                                                       photo           varchar(100) NOT NULL,
+                                                                       photo_thumb     varchar(100) NOT NULL,
+                                                                       name_from_draft varchar(255) DEFAULT NULL,
+                                                                       consent         tinyint(1)   DEFAULT NULL)''')
 
         # Alias
         self.conn.execute('''CREATE TABLE IF NOT EXISTS alias (id     int  NOT NULL PRIMARY KEY,
@@ -231,9 +284,34 @@ class DataTrackerCollector:
                                                                        type   text NOT NULL,
                                                                        FOREIGN KEY (person) REFERENCES person(id))''')
 
+        # Document
+        self.conn.execute('''CREATE TABLE IF NOT EXISTS document (id                 int  NOT NULL PRIMARY KEY,
+                                                                  name               text NOT NULL,
+                                                                  title              text NOT NULL,
+                                                                  pages              int,
+                                                                  words              int,
+                                                                  time               text NOT NULL,
+                                                                  expires            text,
+                                                                  type               text NOT NULL,
+                                                                  rfc                int,
+                                                                  rev                text NOT NULL,
+                                                                  abstract           text NOT NULL,
+                                                                  internal_comments  text NOT NULL,
+                                                                  order_no           int  NOT NULL,
+                                                                  ad                 int,
+                                                                  shepherd           text,
+                                                                  group_id           int,
+                                                                  stream             text,
+                                                                  intended_std_level text,
+                                                                  std_level          text,
+                                                                  uploaded_filename  text NOT NULL,
+                                                                  external_url       text NOT NULL,
+                                                                  FOREIGN KEY (ad) REFERENCES person(id))''')
+
 
     def gather(self):
         self.gather_people()
+        #self.gather_documents()
         self.conn.close()
 
 
